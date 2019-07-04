6fdb5f2d76f01757bc175aed4435ed58adda3a89
pavan reddy <pavankumarmutra@gmail.com>
2019-07-04 15:36:04 +0530

    added files for tf 12

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable rg_name { 
+ default = ""
+ description = "Name of the resource group"
+}
+
+variable location {
+  default = "eastus"
+  description = "Geographic location of the Resource Group"
+} 
