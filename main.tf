9a8379bbd5a43f2210c79909040c1564650413c9
pavan reddy <pavankumarmutra@gmail.com>
2019-07-04 15:27:47 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-    version = "~>1.22"
-subscription_id = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    client_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    client_secret   = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-    tenant_id       = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-}
